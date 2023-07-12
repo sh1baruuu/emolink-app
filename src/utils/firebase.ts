ef666e32164b5245fe38c5a997e8013d9e92f0c8
sh1baruuu <114075679+sh1baruuu@users.noreply.github.com>
2023-07-12 23:43:13 +0800

    Update firebase.ts

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -12,16 +12,6 @@ const firebaseConfig = {
     appId: "1:178562212514:web:76fe4aa16d2367be27adfb"
 };
 
-// const firebaseConfig = {
-//     apiKey: process.env.NEXT_PUBLIC_API_KEY,
-//     authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-//     projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-//     storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-//     messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-//     appId: process.env.NEXT_PUBLIC_APP_ID,
-//     databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL   
-// };
-
 export const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
 export const db = getFirestore(app)
